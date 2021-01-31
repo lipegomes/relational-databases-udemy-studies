50f37c598634c166e539c0bf50cdaa9d0cefbb73
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2021-01-31 15:53:43 -0300

    Add dbs

---
+++ projeto-universidade-u/universidade_u_fisico.sql
@@ -0,0 +1,12 @@
+/* universidade_u_logico: */
+
+CREATE TABLE ALUNO (
+    idade int,
+    sexo char(1),
+    endereco text,
+    telefone varchar(20),
+    data_inscricao_curso date,
+    valor_pago_curso float(10,2),
+    nome varchar(25),
+    ativo_sn int
+);
