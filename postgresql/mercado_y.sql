3af11b708ccdd8af1dbfbddedea47b00a09d8d41
lipegomes <fgdl.py91@gmail.com>
2021-05-06 12:04:56 -0300

    Listando, criando e removendo bancos de dados

---
+++ postgresql/mercado_y.sql
@@ -0,0 +1,12 @@
+-- Listando, criando e removendo bancos de dados
+
+select datname from pg_database;
+
+create database mercado_y;
+
+select datname from pg_database;
+
+drop database mercado_y;
+
+select datname from pg_database;
+
