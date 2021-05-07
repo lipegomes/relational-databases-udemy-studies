1b5f2418a04ad9d91126410f9ccad4c849b10ae2
lipegomes <fgdl.py91@gmail.com>
2021-05-07 10:13:54 -0300

    -- Listando, criando, atualizado e removendo Esquemas (Schemas)

--- postgresql/mercado_y.sql
+++ postgresql/mercado_y.sql
@@ -10,3 +10,17 @@ drop database mercado_y;
 
 select datname from pg_database;
 
+-- Listando, criando, atualizado e removendo Esquemas (Schemas)
+
+select schema_name from information_schema.schemata;
+select nspname from pg_catalog.pg_namespace;
+
+create schema produto;
+alter schema produto rename to produto_abcd;
+drop schema produto_abcd;
+
+create schema produto;
+create schema estoque;
+create schema auditoria;
+create schema venda;
+
